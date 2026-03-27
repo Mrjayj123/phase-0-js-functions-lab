05694aaa7ec01e0ce2723fd0f37b25439a5f1aa1
Mrjayj123/jj <adijayjoel342@gmail.com>
2026-03-27 15:08:32 +0300

    removed one comment

--- calculateDiscountedPrice.js
+++ calculateDiscountedPrice.js
@@ -1,4 +1,4 @@
 function calculateDiscountedPrice(originalPrice, discountPercentage){
     return originalPrice * discountPercentage / 100;
 }
-console.log(calculateDiscountedPrice(10000, 20)); // Output
+console.log(calculateDiscountedPrice(10000, 20)); 
