ce67ff7db5f2af497b617bb6c46ad07e812fa69c
Mrjayj123/jj <adijayjoel342@gmail.com>
2026-03-27 15:30:19 +0300

    Added statements to make code more responsive

--- calculatetax.js
+++ calculatetax.js
@@ -1,6 +1,14 @@
 //create a function called calculateTax that takes in an amount and returns the amount multiplied by 0.10
 function calculateTax(amount){
-    return amount *0.10
+    if (amount > 0){
+        return amount *0.10
+    }
+    else if(amount < 0){
+        return "error: amount cannot be negative"
+    }
+    else(
+        return "Amount is equal to zero"
+    )
 }
 console.log(calculateTax(45))
 //output 4.5
